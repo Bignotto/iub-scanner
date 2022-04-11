bdb8664f68b5d017420c1f0b70179a74dcb0d3aa
Thiago Bignotto <bignotto@gmail.com>
2022-04-11 06:57:23 -0300

    save commit

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../Button";
 
 import { Container, Input } from "./styles";
 
 //TODO: implement input button action
 export default function InputForm() {
+  const [serial, setSerial] = useState("");
+
+  function handleSubmit() {}
+
   return (
     <Container>
-      <Input placeholder="número de série" />
-      <Button title="Inserir" />
+      <form>
+        <Input placeholder="número de série" />
+        <Button title="Inserir" />
+      </form>
     </Container>
   );
 }
